662b2264b36c843db1195b79de9ab25e707a49db
Thiago Porto <thiago.porto@d3.do>
2022-03-11 15:19:41 -0300

    feat: finish categoryController.js

---
+++ controllers/categoryController.js
@@ -0,0 +1,131 @@
+const Category = require('../models/category');
+const Item = require('../models/item');
+
+const { body, validationResult } = require('express-validator');
+
+exports.getAllCategories = async (req, res, next) => {
+  try {
+    const categories = await Category.find();
+    console.log(categories);
+    res.json({ categories });
+  } catch (error) {
+    next(error);
+  }
+};
+
+exports.getCategory = async (req, res, next) => {
+  try {
+    const category = await Category.find({ name: req.params.name }).populate(
+      'items'
+    );
+    res.json({ category });
+  } catch (error) {
+    next(error);
+  }
+};
+
+exports.createCategory = [
+  (req, res, next) => {
+    if (!(req.body.items instanceof Array)) {
+      if (typeof req.body.items === 'undefined') req.body.items = [];
+      else req.body.items = new Array(req.body.items);
+    }
+    next();
+  },
+  body('name')
+    .trim()
+    .isLength({ min: 1 })
+    .escape()
+    .withMessage('Category name must be specified.')
+    .isAlphanumeric()
+    .withMessage('Category name has non-alphanumeric characters.'),
+  body('description')
+    .trim()
+    .isLength({ min: 1 })
+    .escape()
+    .withMessage('Category description must be specified.'),
+  body('items.*').escape(),
+  async (req, res, next) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) return next(errors.array());
+
+    const category = new Category({
+      name: req.body.name.toLowerCase(),
+      description: req.body.description,
+      items: typeof req.body.items === 'undefined' ? [] : req.body.items,
+    });
+
+    try {
+      const createdCategory = await category.save();
+      res.json({ createdCategory });
+    } catch (error) {
+      next(error);
+    }
+  },
+];
+
+exports.updateCategory = [
+  (req, res, next) => {
+    if (!(req.body.items instanceof Array)) {
+      if (typeof req.body.items === 'undefined') req.body.items = [];
+      else req.body.items = new Array(req.body.items);
+    }
+    next();
+  },
+  body('name')
+    .trim()
+    .isLength({ min: 1 })
+    .escape()
+    .withMessage('Category name must be specified.')
+    .isAlphanumeric()
+    .withMessage('Category name has non-alphanumeric characters.'),
+  body('description')
+    .trim()
+    .isLength({ min: 1 })
+    .escape()
+    .withMessage('Category description must be specified.'),
+  body('items.*').escape(),
+  async (req, res, next) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) return next(errors.array());
+
+    try {
+      const [{ _id }] = await Category.find({ name: req.params.name }, '_id');
+
+      const category = new Category({
+        name: req.body.name.toLowerCase(),
+        description: req.body.description,
+        items: typeof req.body.items === 'undefined' ? [] : req.body.items,
+        _id,
+      });
+
+      const updatedCategory = await Category.findByIdAndUpdate(
+        { _id },
+        category
+      );
+
+      res.json({ updatedCategory });
+    } catch (error) {
+      next(error);
+    }
+  },
+];
+
+exports.deleteCategory = async (req, res, next) => {
+  try {
+    const items = await Category.find({}, 'items');
+
+    if (items.length) {
+      return res
+        .status(500)
+        .send('You must delete all items in a Category before deleting it!');
+    }
+
+    await Category.deleteOne({ name: req.body.name });
+    res.json({ message: 'Category deleted successfully!' });
+  } catch (error) {
+    next(error);
+  }
+};
