d540d2361858be1f8f40a2785e9740bf2cb421fd
Thiago Porto <thiago.porto@d3.do>
2022-03-11 13:29:24 -0300

    fix: fix names in app.js

--- app.js
+++ app.js
@@ -8,7 +8,8 @@ const helmet = require('helmet');
 const mongoose = require('mongoose');
 
 const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
+const categoryRouter = require('./routes/category');
+const itemsRouter = require('./routes/items');
 
 const app = express();
 
@@ -32,7 +33,8 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/category', categoryRouter);
+app.use('/items', itemsRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
